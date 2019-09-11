588c74c7d5216ed0b52beee0393a6c57ee8cb918
VladyslavSak <vladyslav.sak@gmail.com>
2019-09-11 22:20:23 +0300

    Updates

---
+++ precache-manifest.f3d38e86d105db5deabdfca6f1150774.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd82e132db011ee985565252aa88d4b7",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "02f56b94801ca4f428c2",
+    "url": "/react_static-list-of-posts/static/css/main.cf453329.chunk.css"
+  },
+  {
+    "revision": "8d72c1bc687b008b68c1",
+    "url": "/react_static-list-of-posts/static/js/2.8047aade.chunk.js"
+  },
+  {
+    "revision": "02f56b94801ca4f428c2",
+    "url": "/react_static-list-of-posts/static/js/main.bf49b5ac.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
